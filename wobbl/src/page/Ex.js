81895e42ae3c3c4997f2115204177d9a81c16ffd
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-21 22:09:43 -0500

    basic route structure

---
+++ wobbl/src/page/Ex.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Ex extends Component {
+  render() {
+    return (<div>Hello this is the example page for the /ex route</div>);
+  }
+}
---
+++ wobbl/src/page/Home.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (<div>Hello this is the example page for the / route aka home</div>);
+  }
+}
