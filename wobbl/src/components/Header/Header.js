5bc807d56b3ca2cc8918f7fb8cb1cb273c9c1384
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-22 01:26:16 -0500

    more formatting

--- wobbl/src/components/Header/Header.js
+++ wobbl/src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./Header.css";
-
+import Icon from "../../img/wobblicon.png";
 export default class Header extends Component {
   render() {
     return (
-      <>
-        <div className="header">This is the HEADER</div>
-      </>
+      <div className="header">
+        <img className ="icon" src={Icon}/>
+        <div className="headerText">
+          This is the HEADER
+        </div>
+      </div>
     );
   }
 }
