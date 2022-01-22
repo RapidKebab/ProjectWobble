6910704ec523c981117de15e190a10812d895603
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-21 23:50:38 -0500

    Update Ex.js

--- wobbl/src/page/Ex.js
+++ wobbl/src/page/Ex.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import Messages from "../components/Messages";
 
 export default class Ex extends Component {
-  
+  constructor(props){
+      super(props);
+      this.state={
+        chat: "deez"
+      }
+  }
   render() {
-    this.setState({chat: ["deez","nuts"]})
     return (
       <>
-        <Messages chat = {this.state.chat}/>
+        {this.state.chat}
         <input type="text"/>
         <button>Send</button>
       </>
