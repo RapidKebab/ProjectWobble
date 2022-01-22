2d0c63e5cc65505ec9175a2455c1087e65c1f6fb
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-22 15:02:25 -0500

    Update message.js

--- backend/src/routes/message.js
+++ backend/src/routes/message.js
@@ -5,7 +5,7 @@ router.route("/").get(async (req, res)=>{
     //TODO get cur messages, if time implement with cocroachDB
 }).post(async (req,res)=>{
     //TODO SEND MESSAGE and emit new message on socket
-    io.emit('newMessage', req.body.message);
+    io.emit('newMessage', {message: req.body.message, zoomId:req.body.zoomId});
     console.log(req.body);
     console.log(`the message: ${req.body.message} has been emitted`);
     res.json("succesful");
