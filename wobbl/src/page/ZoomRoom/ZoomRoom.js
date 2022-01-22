7a796d632a791f309ff68c01ea0632b1b042932a
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-22 15:27:47 -0500

    Update ZoomRoom.js

--- wobbl/src/page/ZoomRoom/ZoomRoom.js
+++ wobbl/src/page/ZoomRoom/ZoomRoom.js
@@ -1,12 +1,15 @@
 import React, { Component, useState } from "react";
 import Messages from "../../components/Messages";
+import { useParams } from "react-router";
 import Msg from "../../components/Msg/Msg";
 import "./ZoomRoom.css";
 
 function Ex(){
+  let {id} = useParams();
   const [chat, setChat] = useState([{id: 1, name: 'deez', points:1},{id: 2, name: 'nuts', points:1}])
     return (
       <>
+        <div>Zoom room id: {id}</div>
         <Messages chat ={chat}/>
         <input type="text"/>
         <button>Send</button>
