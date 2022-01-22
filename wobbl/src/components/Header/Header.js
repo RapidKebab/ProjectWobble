a05b28f1411a33642614a63438da7bded4a91a61
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-22 11:32:02 -0500

    Update Header.js

--- wobbl/src/components/Header/Header.js
+++ wobbl/src/components/Header/Header.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import "./Header.css";
 import Icon from "../../img/wobblicon.png";
-export default class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <img className ="icon" src={Icon}/>
-        <div className="headerText">
-          This is the HEADER
-        </div>
-      </div>
-    );
-  }
+function Header() {
+  return (
+    <div className="header">
+      <img className="icon" src={Icon} />
+      <div className="headerText">This is the HEADER</div>
+    </div>
+  );
 }
+
+export default Header;
