ac17bc4cacc648fa83618a2a633c1608670a851c
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-22 14:01:01 -0500

    socket basic messaging to all

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ var app = express();
 var cors = require('cors');
 const http = require('http');
 const server = http.createServer(app);
+const bp = require('body-parser');
+
 
 global.io = require("socket.io")(server, {
     cors: {
@@ -21,6 +23,10 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(bp.json());
+
 
 
 //routes
