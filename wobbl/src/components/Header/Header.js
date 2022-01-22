a00850da65c8c25a264e2c69a59adb7e36234d16
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-22 15:24:08 -0500

    header icon links to home

--- wobbl/src/components/Header/Header.js
+++ wobbl/src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 import Icon from "../../img/wobblicon.png";
 function Header() {
+  let navigate = useNavigate();
   return (
     <div className="header">
-      <img className="icon" src={Icon} />
+      <img
+        className="icon"
+        src={Icon}
+        onClick={() => {
+          navigate("/");
+        }}
+      />
       <div className="headerText">This is the HEADER</div>
     </div>
   );
