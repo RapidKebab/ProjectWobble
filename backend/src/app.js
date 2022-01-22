077de0716633f1484bd8156876a5bcd3f6339dfa
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-22 10:52:49 -0500

    basic sockets

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = 5050;
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
 
 app.get('/api/test', function (req, res) {
     res.send('This is a test API!');
 });
 
-app.listen(5050, function () {
+server.listen(port, function () {
     console.log('Backend server listening on port 5050');
 });
