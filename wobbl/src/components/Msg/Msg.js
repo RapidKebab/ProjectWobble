a8a48a542774ca1559af8956ee643869977c6915
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-23 01:20:26 -0500

    up and down with button animation

--- wobbl/src/components/Msg/Msg.js
+++ wobbl/src/components/Msg/Msg.js
@@ -2,25 +2,53 @@ import React, { useState } from "react";
 import "./Msg.css";
 import UpIcon from "../../img/uparrow.png";
 import DownIcon from "../../img/downarrow.png";
+import { motion } from "framer-motion";
 
 function Msg({ msg }) {
   const [points, setPoints] = useState(320);
   console.log(msg);
-  const style = {
-    backgroundColor: `hsl(14, ${points/10}%, 50%)`,
-    width: "100px",
-    height: "100px",
-  };
+
+
+  function upVote() {
+    console.log("upvote");
+  }
+
+  function downVote() {
+    console.log("downvote");
+  }
 
   return (
     <>
       <div className="msg">
         <div className="msgtxt">{msg}</div>
-        <div style={style} />
         <div className="buttons">
           +1
-          <img className="up" src={UpIcon} />
-          <img className="down" src={DownIcon} />
+          <motion.img
+            className="up"
+            src={UpIcon}
+            onClick={() => {
+              upVote();
+            }}
+            whileHover={{
+              scale: 1.1,
+            }}
+            whileTap={{
+              scale: 0.9,
+            }}
+          />
+          <motion.img
+            className="down"
+            src={DownIcon}
+            onClick={() => {
+              downVote();
+            }}
+            whileHover={{
+              scale: 1.1,
+            }}
+            whileTap={{
+              scale: 0.9,
+            }}
+          />
         </div>
       </div>
     </>
