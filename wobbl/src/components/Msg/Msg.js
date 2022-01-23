ec51473590b0a40cd495651a33f4ac816693e5d7
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-23 00:48:20 -0500

    Update Msg.js

--- wobbl/src/components/Msg/Msg.js
+++ wobbl/src/components/Msg/Msg.js
@@ -1,34 +1,28 @@
-import React from "react";
-import "./Msg.css"
+import React, { useState } from "react";
+import "./Msg.css";
 import UpIcon from "../../img/uparrow.png";
 import DownIcon from "../../img/downarrow.png";
 
-function Msg({msg}) {
+function Msg({ msg }) {
+  const [points, setPoints] = useState(320);
   console.log(msg);
+  const style = {
+    backgroundColor: `hsl(14, ${points/10}%, 50%)`,
+    width: "100px",
+    height: "100px",
+  };
+
   return (
     <>
       <div className="msg">
         <div className="msgtxt">{msg}</div>
-        <div
-        style={{
-        backgroundColor: hsl(14, var%, 50%);,
-        width: '100px',
-        height: '100px'
-        }}
-        />
+        <div style={style} />
         <div className="buttons">
-        +1
-        <img
-        className="up"
-        src={UpIcon}
-        />
-        <img
-        className="down"
-        src={DownIcon}
-        />
+          +1
+          <img className="up" src={UpIcon} />
+          <img className="down" src={DownIcon} />
         </div>
       </div>
-
     </>
   );
 }
